0e3da800de69cca3924cff04acc2b00f28f6d0c1
joeygeofrey <joeygeof.dev@gmail.com>
2022-11-17 13:07:02 -0500

    Removed starter codes and added new smart contract

---
+++ contracts/Dapp.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract Dapp {
+    constructor() {
+        console.log("Yo yo, sup! Call me contract and I am kinda smart.. hehe");
+    }
+}
