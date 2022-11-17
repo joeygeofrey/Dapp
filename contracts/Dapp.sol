89ea0754d6cff46457a109e5ce6a93f4193309df
joeygeofrey <joeygeof.dev@gmail.com>
2022-11-17 13:13:22 -0500

    Updated SC to store the 'dapps'

--- contracts/Dapp.sol
+++ contracts/Dapp.sol
@@ -4,8 +4,20 @@ pragma solidity ^0.8.17;
 
 import "hardhat/console.sol";
 
-contract Dapp {
+contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
-        console.log("Yo yo, sup! Call me contract and I am kinda smart.. hehe");
+        console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
