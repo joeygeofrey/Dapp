ed2164df700c5e5c7cdb8ccc36d0324efbd97e7f
joeygeofrey <joeygeof.dev@gmail.com>
2022-11-17 14:24:55 -0500

    Updated deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const main = async () => {
+    const [deployer] = await hre.ethers.getSigners();
+    const accountBalance = await deployer.getBalance();
+  
+    console.log("Deploying contracts with account: ", deployer.address);
+    console.log("Account balance: ", accountBalance.toString());
+  
+    const dappContractFactory = await hre.ethers.getContractFactory("Dapp");
+    const dappContract = await dappContractFactory.deploy();
+    await dappContract.deployed();
+  
+    console.log("Dapp address: ", dappContract.address);
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
